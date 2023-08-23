aa8ae2d77d13656f58b2e25267cc44ac98a74794
Sebastian Mori <mori.sebastian@outlook.com>
2023-08-23 13:34:28 -0500

    creando clase empresa

---
+++ app/src/main/java/com/bastian/androidjavaentregable/Empresa.java
@@ -0,0 +1,31 @@
+package com.bastian.androidjavaentregable;
+
+public class Empresa {
+    private int id;
+    private String razonSocial;
+    private String ruc;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getRazonSocial() {
+        return razonSocial;
+    }
+
+    public void setRazonSocial(String razonSocial) {
+        this.razonSocial = razonSocial;
+    }
+
+    public String getRuc() {
+        return ruc;
+    }
+
+    public void setRuc(String ruc) {
+        this.ruc = ruc;
+    }
+}
