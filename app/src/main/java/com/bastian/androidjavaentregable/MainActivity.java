26b8b03d8c4b1994b1b6f28852def98cac87a605
Sebastian Mori <mori.sebastian@outlook.com>
2023-08-24 11:21:17 -0500

    actualizando clase principal, implementando funcionamiento de los botones

--- app/src/main/java/com/bastian/androidjavaentregable/MainActivity.java
+++ app/src/main/java/com/bastian/androidjavaentregable/MainActivity.java
@@ -3,15 +3,19 @@ package com.bastian.androidjavaentregable;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import java.util.ArrayList;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnClickListener {
     private EmpresaDAO objEmpresa;
     EditText txtRazonSocial, txtRUC;
     ArrayList<Empresa> lista = null;
+    Button btnRegistrar, btnListar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,35 +23,50 @@ public class MainActivity extends Activity {
 
         txtRazonSocial = (EditText) findViewById(R.id.etRazonS);
         txtRUC = (EditText) findViewById(R.id.etRuc);
+        btnRegistrar = (Button) findViewById(R.id.btnRegistrar);
+        btnListar = (Button) findViewById(R.id.btnListar);
+        btnRegistrar.setOnClickListener(this);
+        btnListar.setOnClickListener(this);
+
         objEmpresa = new EmpresaDAO(this);
         objEmpresa.open();
     }
 
-    public void Grabar(View view){
+    public void Grabar() {
         String razonSocial = txtRazonSocial.getText().toString();
         String ruc = txtRUC.getText().toString();
         long i = 0;
         i = objEmpresa.Insertar(razonSocial, ruc);
 
-        if(i==0){
-            Toast.makeText(getApplicationContext(),"Registro No Insertado", 1000).show();
+        if (i == 0) {
+            Toast.makeText(getApplicationContext(), "Registro No Insertado", Toast.LENGTH_LONG).show();
         } else {
-            Toast.makeText(getApplicationContext(),"Registro Insertado", 1000).show();
+            Toast.makeText(getApplicationContext(), "Registro Insertado", Toast.LENGTH_LONG).show();
             txtRazonSocial.getText().clear();
             txtRUC.getText().clear();
             txtRazonSocial.requestFocus();
         }
     }
 
-    public void cargarTabla(View view){
+    public void cargarTabla() {
         String acum = "";
         try {
             lista = objEmpresa.ListadoGeneral();
-            for(Empresa obj:lista){
-                acum += obj.getRazonSocial()+" "+obj.getRuc()+"\n";
+            for (Empresa obj : lista) {
+                acum += obj.getRazonSocial() + " " + obj.getRuc() + "\n";
             }
-            Toast.makeText(getApplicationContext(), acum, 2000).show();
-        } catch (Exception e){
+            Toast.makeText(getApplicationContext(), acum, Toast.LENGTH_LONG).show();
+        } catch (Exception e) {
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (btnRegistrar == view) {
+            Grabar();
+        }
+        if (btnRegistrar == view) {
+            cargarTabla();
         }
     }
 }
